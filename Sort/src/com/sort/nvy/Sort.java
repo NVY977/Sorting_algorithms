fa48028203da0f7d8ee4cc0a6d87195b25e526fd
unknown <nvy977@gmail.com>
2022-05-31 22:34:57 +0300

    Sort

---
+++ Sort/src/com/sort/nvy/Sort.java
@@ -0,0 +1,110 @@
+package com.sort.nvy;
+import java.util.Arrays;
+
+// Algorithm complexity : O(nlog(n))
+public class Sort {
+
+    public static void bubbleSort(int[] array) {
+        boolean isSorted = false;
+        int buf;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    isSorted = false;
+                    buf = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = buf;
+                }
+            }
+        }
+    }
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low >= high) return; //завершить выполнение если уже нечего делить
+
+        // выбрать опорный элемент
+        int middle = low + (high - low) / 2;
+        int pivot = array[middle];
+
+        // разделить на подмассивы, который больше и меньше опорного элемента
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            //меняем местами
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        // вызов рекурсии для сортировки левой и правой части
+        if (low < j)
+            quickSort(array, low, j);
+
+        if (high > i)
+            quickSort(array, i, high);
+    }
+
+    static void heapify(int[] array, int length, int i) {
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+        int largest = i;
+
+        // если левый дочерний больше родительского
+        if (leftChild < length && array[leftChild] > array[largest]) {
+            largest = leftChild;
+        }
+
+        // если правый дочерний больше родительского
+        if (rightChild < length && array[rightChild] > array[largest]) {
+            largest = rightChild;
+        }
+
+        // если должна произойти замена
+        if (largest != i) {
+            int temp = array[i];
+            array[i] = array[largest];
+            array[largest] = temp;
+            heapify(array, length, largest);
+        }
+    }
+
+    public static void heapSort(int[] array) {
+        if (array.length == 0) return;
+
+        // Строим кучу
+        int length = array.length;
+        // проходим от первого без ответвлений к корню
+        for (int i = length / 2 - 1; i >= 0; i--)
+            heapify(array, length, i);
+
+        for (int i = length - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] x = {8, 0, 4, 7, 3, 7, 10, 12, -3};
+        System.out.println("-------------- Sort --------------");
+        System.out.println("Before: " + Arrays.toString(x));
+        int begin = 0;
+        int end = x.length - 1;
+        quickSort(x, begin, end);
+        // bubbleSort(x);
+        // heapSort(x);
+        System.out.println("After:  " + Arrays.toString(x));
+    }
+}
